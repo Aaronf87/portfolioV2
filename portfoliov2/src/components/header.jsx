79340ac7478f1c8827fd7a4334ceaf8ed17a2b4e
Aaron Flores <aaron_flores08@yahoo.com>
2023-10-23 17:49:12 -0500

    added nav tailwind

--- portfoliov2/src/components/header.jsx
+++ portfoliov2/src/components/header.jsx
@@ -1,16 +1,30 @@
-export default function header() {
+export default function Header() {
     return (
         <div>
-     <nav>
-        <ul>
-            <li><a href="/">Home</a></li>
-            <li><a href="/about">About Me</a></li>
-            <li><a href="/contact">Contact Me</a></li>
-            <li><a href="/projects">Projects</a></li>
-        </ul>
-     </nav>
+            <nav className="bg-gray-800">
+                <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
+                    <div className="relative flex h-20 items-center justify-between tracking-tight">
+                        <h1 style={{ color: "rgb(233, 226, 226)", backgroundColor: "rgba(28, 21, 21, 0.15)", backdropFilter: "blur(10px)" }}>By Aaron Flores</h1>
+                        <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
+                            <button type="button" className="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white" aria-controls="mobile-menu" aria-expanded="false">
+                                <span className="absolute -inset-0.5"></span>
+                                <span className="sr-only">Open main menu</span>
+                            </button>
+                        </div>
+                        <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
+                            <div className="hidden sm:ml-6 sm:block">
+                                <div className="flex space-x-4">
+                                    <a href="/about" className="bg-gray-900 text-white rounded-md px-3 py-2 text-sm font-medium text-3xl" aria-current="page">About Me</a>
+                                    <a href="/projects" className="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium text-3xl">Projects</a>
+                                    <a href= "/contact" className="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium text-3xl">Contact Me</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </nav>
         </div>
-    )
-    }
+    );
+}
 
 
--- portfoliov2/src/pages/About.jsx
+++ portfoliov2/src/pages/About.jsx
@@ -1,8 +1,8 @@
 export default function About() {
     return (
-        <div>
-            <h1 className = "text-3xl font-bold underline text-red-600">Hi There,  My name is Aaron</h1>
-            <p>I am a developer from San Antonio, Texas.  I enjoy all things tech hardware and software.  My goal is to find and inject efficenty in the world of technology to benefit people in their quest to solve real world problems.  The ability to innovate and pioneer bigger and better is a dream I continue to pursue.</p>
+        <div classNameName = "bg-gray-800">
+            <h1 classNameName = "text-3xl font-bold tracking-tight text-white sm:text-4xl">Hi There,  My name is Aaron</h1>
+            <p classNameName = "mt-4 text-white">I am a developer from San Antonio, Texas.  I enjoy all things tech hardware and software.  My goal is to find and inject efficency in the world of technology to benefit people in their quest to solve real world problems.  The ability to innovate and pioneer bigger and better is a dream I continue to pursue.</p>
         </div>
     )
 
