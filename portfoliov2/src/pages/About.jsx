f6f9fb7128476f66a318f9b8d1408425c63118ca
Aaron Flores <aaron_flores08@yahoo.com>
2023-10-23 18:01:24 -0500

    changed bg-black on about page

--- portfoliov2/src/pages/About.jsx
+++ portfoliov2/src/pages/About.jsx
@@ -1,10 +1,18 @@
 export default function About() {
     return (
-        <div classNameName = "bg-gray-800">
-            <h1 classNameName = "text-3xl font-bold tracking-tight text-white sm:text-4xl">Hi There,  My name is Aaron</h1>
-            <p classNameName = "mt-4 text-white">I am a developer from San Antonio, Texas.  I enjoy all things tech hardware and software.  My goal is to find and inject efficency in the world of technology to benefit people in their quest to solve real world problems.  The ability to innovate and pioneer bigger and better is a dream I continue to pursue.</p>
+        <div className="flex flex-col min-h-screen bg-black">
+          
+                <h1 className="text-3xl font-bold tracking-tight text-white sm:text-4xl">
+                    Hi There, My name is Aaron
+                </h1>
+                <p className="mt-4 text-white">
+                    I am a developer from San Antonio, Texas. I enjoy all things tech
+                    hardware and software. My goal is to find and inject efficiency in the
+                    world of technology to benefit people in their quest to solve real
+                    world problems. The ability to innovate and pioneer bigger and better
+                    is a dream I continue to pursue.
+                </p>
+         
         </div>
-    )
-
-
+    );
 }
