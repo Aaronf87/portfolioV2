1a660498eb263938662272563e3415f008a5b2e3
Aaron Flores <aaron_flores08@yahoo.com>
2023-10-23 19:13:21 -0500

    added padding to pages

--- portfoliov2/src/pages/Contact.jsx
+++ portfoliov2/src/pages/Contact.jsx
@@ -1,6 +1,6 @@
 export default function Contact() {
 return (
-    <div className="flex flex-col min-h-screen bg-black background text-white">
+    <div className="flex flex-col min-h-screen bg-black background text-white p-10">
  <h1 className="text-3xl font-bold tracking-tight text-white sm:text-4xl">Contact Me</h1>
 <a href="https://github.com/Aaronf87">My Github
 <p>aaron_flores08@yahoo.com</p>
