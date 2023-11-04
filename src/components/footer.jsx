bc108eb994718cf1f093cecb8fca63be3d037cf0
Aaron Flores <aaron_flores08@yahoo.com>
2023-11-03 23:05:17 -0500

    footer applied date

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,10 +1,9 @@
 export default function Footer(){
-return (
-<footer class ="bg-gray-800 max-h-80 py-4">
-        <h2 class = "text-white p-2 flex justify-center"> Contact Me</h2>
-        <section class="flex justify-center space-x-6">
-            <a class = "text-white" href="https://github.com/Aaronf87">My Github
-              </a></section>
-    </footer>
-)
-}
+  return (
+  <footer class ="text-2xl bg-gray-800 max-h-80 py-4 text-white">
+    <p>&copy; {new Date().getFullYear()} portfolio by Aaron Flores</p>
+         
+      </footer>
+  )
+  }
+  
